85539deaee9dde1066dd3220f8a1cd7532c0d6d9
Arbaaz Ali <arbaaz.py@gmail.com>
2022-12-21 17:14:48 +0530

    first commit

---
+++ backend/students/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework import routers
+
+from .views import StudentViewSet
+
+router = routers.DefaultRouter()
+router.register("students", StudentViewSet)
+
+urlpatterns = router.urls
